8999c121448b9605664bae66b4089f1a7e202c60
Jon Curtis <jonathan.curtis@huddle.com>
2013-01-30 09:18:32 +0000

    initial commit

---
+++ RockPaperScissors/Game.fs
@@ -0,0 +1,16 @@
+﻿module Game
+
+type Hand = Rock | Paper | Scissors
+type Result = Win | Lose | Draw
+
+let game (you, them) =
+    match (you, them) with
+    | (Rock, Rock) -> Draw
+    | (Rock, Paper) -> Lose
+    | (Rock, Scissors) -> Win
+    | (Paper, Rock) -> Win
+    | (Paper, Paper) -> Draw
+    | (Paper, Scissors) -> Lose
+    | (Scissors, Rock) -> Lose
+    | (Scissors, Paper) -> Win
+    | (Scissors, Scissors) -> Draw
---
+++ RockPaperScissors/Program.fs
@@ -0,0 +1,28 @@
+﻿module program
+
+open System
+open Game
+
+exception IncorrectNumberOfArguments
+exception HandNotRecognized of string
+
+let parseCommandLineArgs args =
+    let parseHand hand =
+        match hand with
+        | "rock" -> Rock
+        | "paper" -> Paper
+        | "scissors" -> Scissors
+        | _ -> raise (HandNotRecognized hand)
+    match args with
+    | [| _; playerHand; opponentHand |] -> (parseHand playerHand, parseHand opponentHand)
+    | _ -> raise IncorrectNumberOfArguments
+
+let outputGame result =
+    let output result =
+        match result with
+        | Win -> "You win"
+        | Lose -> "You lose"
+        | Draw -> "It's a draw"
+    Console.WriteLine(output result)
+
+outputGame (game (parseCommandLineArgs (System.Environment.GetCommandLineArgs())))
